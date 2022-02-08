6c81874a0e535fe0ae305b319247d1604dbda9a0
yayanrw <yayanraw@gmail.com>
2022-02-08 19:15:00 +0700

    add account cont

---
+++ JwtAuth/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using JwtAuth.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuth.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtSettings jwtSettings;
+        public AccountController(JwtSettings jwtSettings)
+        {
+            this.jwtSettings = jwtSettings;
+        }
+        private IEnumerable<Users> logins = new List<Users>() {
+            new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "adminakp@gmail.com",
+                        UserName = "Admin",
+                        Password = "Admin",
+                },
+                new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "adminakp@gmail.com",
+                        UserName = "User1",
+                        Password = "Admin",
+                }
+        };
+        [HttpPost]
+        public IActionResult GetToken(UserLogins userLogins)
+        {
+            try
+            {
+                var Token = new UserTokens();
+                var Valid = logins.Any(x => x.UserName.Equals(userLogins.UserName, StringComparison.OrdinalIgnoreCase));
+                if (Valid)
+                {
+                    var user = logins.FirstOrDefault(x => x.UserName.Equals(userLogins.UserName, StringComparison.OrdinalIgnoreCase));
+                    Token = JwtHelpers.JwtHelpers.GenTokenkey(new UserTokens()
+                    {
+                        EmailId = user.EmailId,
+                        GuidId = Guid.NewGuid(),
+                        UserName = user.UserName,
+                        Id = user.Id,
+                    }, jwtSettings);
+                }
+                else
+                {
+                    return BadRequest("wrong password");
+                }
+                return Ok(Token);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// Get List of UserAccounts
+        /// </summary>
+        /// <returns>List Of UserAccounts</returns>
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult GetList()
+        {
+            return Ok(logins);
+        }
+    }
+}
